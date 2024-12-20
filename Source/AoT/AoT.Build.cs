1414223bf3b9e89eff2aa6d5f87d4955c0e97180
Piotrr0 <piotrr.nawalany@gmail.com>
2024-12-20 13:40:23 +0100

    Scrap project

--- Source/AoT/AoT.Build.cs
+++ Source/AoT/AoT.Build.cs
@@ -8,7 +8,7 @@ public class AoT : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
