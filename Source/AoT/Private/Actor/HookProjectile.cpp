d7efc0a6c2341055db272c43a32831e0e8a6bbef
Piotrr0 <piotrr.nawalany@gmail.com>
2025-01-14 22:50:53 +0100

    Attach cable to hook projectile

--- Source/AoT/Private/Actor/HookProjectile.cpp
+++ Source/AoT/Private/Actor/HookProjectile.cpp
@@ -4,6 +4,9 @@
 #include "Actor/HookProjectile.h"
 #include "AoTGameplayTags.h"
 #include "AbilitySystem/Abilities/HookAbility.h"
+#include "Kismet/GameplayStatics.h"
+#include "Interfaces/PlayerInterface.h"
+#include "CableComponent.h"
 
 AHookProjectile::AHookProjectile()
 {
@@ -21,6 +24,22 @@ void AHookProjectile::PrematureReturn()
 void AHookProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	if (PlayerPawn->Implements<UPlayerInterface>())
+	{
+		if (FireSocket.MatchesTagExact(FAoTGameplayTags::Get().CombatSocket_LeftGear))
+		{
+			UCableComponent* LeftCable = IPlayerInterface::Execute_GetLeftCable(PlayerPawn);
+			LeftCable->SetVisibility(true);
+			LeftCable->SetAttachEndTo(this, FName(""));
+		}
+		else if (FireSocket.MatchesTagExact(FAoTGameplayTags::Get().CombatSocket_RightGear))
+		{
+			UCableComponent* RightCable = IPlayerInterface::Execute_GetRightCable(PlayerPawn);
+			RightCable->SetVisibility(true);
+			RightCable->SetAttachEndTo(this, FName(""));
+		}
+	}
 }
 
 void AHookProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
